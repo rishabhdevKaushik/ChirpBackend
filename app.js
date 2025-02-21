3ef3fe654cb245ead94eaaf17e41eebd47c31a71
rishabhdevKaushik <rishabhdevkaushik87@gmail.com>
2025-02-21 08:28:20 +0530

    Changed cors origin

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "*",
+        origin: process.env.CLIENT_URL || "http://localhost:3000",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
